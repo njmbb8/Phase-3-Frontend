6342f0db465194a6e54b00103a6ce04428e118e1
njmbb8 <nmcgr001@gmail.com>
2021-12-03 11:24:20 -0500

    Add a new order is now displayed on the orders list instead of 0

--- src/OrderListItem.js
+++ src/OrderListItem.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 function OrderListItem({order}){
     return(
         <Link className="listRow" to={order.id!==0?`/orders/${order.id}`:'/addorder'} >
-            <p>{order.id}</p>
-            <p>{order.updated_at}</p>
+            <p>{order.id === 0? 'Add a new order' : order.id}</p>
+            <p>{order.id === 0? '' : order.updated_at}</p>
         </Link>
     )
 }
